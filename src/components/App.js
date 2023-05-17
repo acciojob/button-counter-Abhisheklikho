bd48b1d5cb7afee3bc0af3b2842c6022d143bbd7
Abhisheklikho <111559519+Abhisheklikho@users.noreply.github.com>
2023-05-17 08:56:44 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import { count } from "console";
+import { react } from "babel-types";
 
 const App = () => {
+ [cont,setCont]=useState("0");
   return (
+    
     <div>
+    <button onClick={{setCont}}></button>
         {/* Do not remove the main div */}
+        <p>Button clicked {cont} times</p>
     </div>
   )
 }
